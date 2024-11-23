7c04b676d3e3840af4783caba6a2bcc7dcaab824
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2024-11-23 18:27:35 -0300

    iniciando projeto de microservicos usando kafka

---
+++ src/main/java/com/fernando/microservice_kafka/MicroserviceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.microservice_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceKafkaApplication.class, args);
+	}
+
+}
